fcbb7c22061ac94c9d3657a41bdb8ff62528787b
robertotriunfo <robertotriunfo@gmail.com>
2025-08-02 22:16:35 +0100

    chore: add Jest configuration and setup for testing

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.test.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+};
